ba73666eee9f763589dd68799c750c2530282f41
Mitsuharu Seki <mitsu1986@gmail.com>
2018-04-27 08:04:54 +0900

    fix apt-get

--- scripts/setup_chroot.sh
+++ scripts/setup_chroot.sh
@@ -2,7 +2,7 @@
 
 apt-add-repository universe
 apt-add-repository multiverse
-apt-get update
+apt-get -yq update
 apt-get -yq install gcc-aarch64-linux-gnu git make gcc bc device-tree-compiler u-boot-tools \
   ncurses-dev qemu-user-static wget cpio kmod squashfs-tools bison flex libssl-dev patch \
   xz-utils b43-fwcutter bzip2 ccache
