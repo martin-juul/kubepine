9545d06156a61dd39270e5ca1858286d6b2d8076
Mitsuharu Seki <mitsu1986@gmail.com>
2018-04-27 06:44:51 +0900

    Fix Shell Scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -eu
 
-export BUILDDIR=/build
+BUILDDIR=/build
+OUTDIR=$BUILDDIR/output
 
 apt-add-repository universe
 apt-add-repository multiverse
@@ -22,8 +23,11 @@ git clone --depth 1 https://git.kernel.org/pub/scm/linux/kernel/git/firmware/lin
 git clone --depth 1 git://git.denx.de/u-boot.git u-boot
 wget -q http://dl-cdn.alpinelinux.org/alpine/v3.7/releases/aarch64/alpine-uboot-3.7.0-aarch64.tar.gz
 wget -q http://mirror2.openwrt.org/sources/broadcom-wl-4.150.10.5.tar.bz2
-cd $BUILDDIR/alpine && tar zxf $BUILDDIR/alpine-uboot-3.7.0-aarch64.tar.gz
-cd $BUILDDIR/initramfs && gunzip -c $BUILDDIR/alpine/boot/initramfs-vanilla | cpio -i
+
+cd $BUILDDIR/alpine
+tar zxf $BUILDDIR/alpine-uboot-3.7.0-aarch64.tar.gz
+cd $BUILDDIR/initramfs
+gunzip -c $BUILDDIR/alpine/boot/initramfs-vanilla | cpio -i
 
 export CROSS_COMPILE=aarch64-linux-gnu-
 export ARCH=arm64
@@ -34,8 +38,8 @@ make rpi_3_defconfig && make
 
 cd $BUILDDIR/kernel
 make kernelversion > $BUILDDIR/kernelversion
-export KERNELVERSION=`cat $BUILDDIR/kernelversion`
-echo KERNELVERSION=$KERNELVERSION
+KERNELVERSION=$(cat $BUILDDIR/kernelversion)
+echo KERNELVERSION="$KERNELVERSION"
 make O=$BUILDDIR/kernel-build bcmrpi3_defconfig
 scripts/kconfig/merge_config.sh -O $BUILDDIR/kernel-build/ $BUILDDIR/kernel-build/.config $BUILDDIR/scripts/config.add
 make O=$BUILDDIR/kernel-build -j8
@@ -59,29 +63,29 @@ mkimage -A arm64 -O linux -T script -C none -a 0 -e 0 -n "raspberry-pi" -d $BUIL
 cp -R $BUILDDIR/initramfs/lib/modules $BUILDDIR/modloop/lib/
 mksquashfs $BUILDDIR/modloop/lib/ $BUILDDIR/modloop-rpi3 -comp xz -Xdict-size 100%
 
-mkdir -p $BUILDDIR/output/boot
-mkdir -p $BUILDDIR/output/firmware
-cp $BUILDDIR/kernel-build/arch/arm64/boot/Image $BUILDDIR/output/boot/Image
-cp $BUILDDIR/kernel-build/arch/arm64/boot/dts/broadcom/bcm2710-rpi-3-b.dtb $BUILDDIR/output/
-cp $BUILDDIR/kernel-build/arch/arm64/boot/dts/broadcom/bcm2710-rpi-3-b-plus.dtb $BUILDDIR/output/
-cp $BUILDDIR/initramfs-rpi3 $BUILDDIR/output/boot/
-cp $BUILDDIR/modloop-rpi3 $BUILDDIR/output/boot/
-cp $BUILDDIR/boot.scr $BUILDDIR/output/boot/
-cp $BUILDDIR/pi-firmware/boot/bootcode.bin $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/start.elf $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/fixup.dat $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/start_cd.elf $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/fixup_cd.dat $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/start_x.elf $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/fixup_x.dat $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/start_db.elf $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/fixup_db.dat $BUILDDIR/output/
-cp $BUILDDIR/pi-firmware/boot/bcm2710-rpi-cm3.dtb $BUILDDIR/output/
-cp $BUILDDIR/alpine/alpine.apkovl.tar.gz $BUILDDIR/output/
-cp -R $BUILDDIR/alpine/apks $BUILDDIR/output/
-cp -R $BUILDDIR/modloop/lib/firmware/brcm $BUILDDIR/output/firmware
-cp -R $BUILDDIR/modloop/lib/firmware/b43 $BUILDDIR/output/firmware
-cp $BUILDDIR/u-boot/u-boot.bin $BUILDDIR/output/boot/
-cp $BUILDDIR/scripts/config.txt $BUILDDIR/output/
-cp $BUILDDIR/scripts/cmdline.txt $BUILDDIR/output/
-cd $BUILDDIR/output && tar Jcf $BUILDDIR/alpine-rpi-3.7.0-aarch64-$KERNELVERSION.tar.xz .
+mkdir -p $OUTDIR/boot
+mkdir -p $OUTDIR/firmware
+cp $BUILDDIR/kernel-build/arch/arm64/boot/Image $OUTDIR/boot/Image
+cp $BUILDDIR/kernel-build/arch/arm64/boot/dts/broadcom/bcm2710-rpi-3-b.dtb $OUTDIR/
+cp $BUILDDIR/kernel-build/arch/arm64/boot/dts/broadcom/bcm2710-rpi-3-b-plus.dtb $OUTDIR/
+cp $BUILDDIR/initramfs-rpi3 $OUTDIR/boot/
+cp $BUILDDIR/modloop-rpi3 $OUTDIR/boot/
+cp $BUILDDIR/boot.scr $OUTDIR/boot/
+cp $BUILDDIR/pi-firmware/boot/bootcode.bin $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/start.elf $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/fixup.dat $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/start_cd.elf $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/fixup_cd.dat $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/start_x.elf $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/fixup_x.dat $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/start_db.elf $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/fixup_db.dat $OUTDIR/
+cp $BUILDDIR/pi-firmware/boot/bcm2710-rpi-cm3.dtb $OUTDIR/
+cp $BUILDDIR/alpine/alpine.apkovl.tar.gz $OUTDIR/
+cp -R $BUILDDIR/alpine/apks $OUTDIR/
+cp -R $BUILDDIR/modloop/lib/firmware/brcm $OUTDIR/firmware
+cp -R $BUILDDIR/modloop/lib/firmware/b43 $OUTDIR/firmware
+cp $BUILDDIR/u-boot/u-boot.bin $OUTDIR/boot/
+cp $BUILDDIR/scripts/config.txt $OUTDIR/
+cp $BUILDDIR/scripts/cmdline.txt $OUTDIR/
+cd $OUTDIR && tar Jcf "$BUILDDIR/alpine-rpi-3.7.0-aarch64-$KERNELVERSION.tar.xz" .
--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -eu
 
-cd $CHROOTDIR
-echo CurrentDirectory: $(pwd)
+cd "$CHROOTDIR"
+echo CurrentDirectory: "$(pwd)"
 debootstrap --arch amd64 --variant=buildd --no-check-gpg --include=software-properties-common,sudo bionic . http://mirror.genesisadaptive.com/ubuntu/
-mkdir -p $CHROOTDIR/build
-cp -R $TRAVIS_BUILD_DIR/scripts $CHROOTDIR/build/
-mount -t sysfs sysfs $CHROOTDIR/sys/
-mount -t proc proc $CHROOTDIR/proc/
-mount -o bind /dev $CHROOTDIR/dev/
-mount -o bind /dev/pts $CHROOTDIR/dev/pts
+mkdir -p "$CHROOTDIR/build"
+cp -R "$TRAVIS_BUILD_DIR/scripts" "$CHROOTDIR/build/"
+mount -t sysfs sysfs "$CHROOTDIR/sys/"
+mount -t proc proc "$CHROOTDIR/proc/"
+mount -o bind /dev "$CHROOTDIR/dev/"
+mount -o bind /dev/pts "$CHROOTDIR/dev/pts"
