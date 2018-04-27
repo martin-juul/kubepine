56b75e9cb30d87ac648cc0d419208cdfa603206a
chalharu <mitsu1986@gmail.com>
2018-04-27 10:47:22 +0900

    Update cleanup.sh

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -4,6 +4,7 @@ umount -l $CHROOTDIR/dev/pts
 umount -l $CHROOTDIR/dev
 umount -l $CHROOTDIR/sys
 umount -l $CHROOTDIR/proc
+umount -l $CHROOTDIR/ccache
 umount -l $CHROOTDIR/build/scripts
 rm -rf $CHROOTDIR
 rm -rf $CACHEDIR/output
