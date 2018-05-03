a824a2c58f101d7013984a659e2bb01724c51f23
chalharu <mitsu1986@gmail.com>
2018-05-04 06:42:43 +0900

    Update setup_chroot.sh

--- scripts/setup_chroot.sh
+++ scripts/setup_chroot.sh
@@ -5,5 +5,5 @@ apt-add-repository multiverse
 apt-get -yq update
 apt-get -yq install gcc-aarch64-linux-gnu git make gcc bc device-tree-compiler u-boot-tools \
   ncurses-dev qemu-user-static wget cpio kmod squashfs-tools bison flex libssl-dev patch \
-  xz-utils b43-fwcutter bzip2 ccache awk
+  xz-utils b43-fwcutter bzip2 ccache gawk
 apt-get -yq clean
